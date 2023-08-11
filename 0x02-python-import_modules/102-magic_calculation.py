d44882ab8d7e29b1375467b903b080b5c9879bfe
johnyREx <johnkwofie99@icloud.com>
2023-08-11 02:06:33 -0700

    nah mean

---
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import magic_calculation_102
+
+
+def magic_calculation(a, b):
+    add = magic_calculation_102.add
+    sub = magic_calculation_102.sub
+    if a < b:
+        c = add(a, b)
+        for i in range(4, 6):
+            c = add(c, i)
+        return (c)
+    else:
+        return (sub(a, b))
