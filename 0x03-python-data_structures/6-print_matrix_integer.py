a825405c89cb81f256bea5182e2ab3fd91aaa3e7
johnyREx <johnkwofie99@icloud.com>
2023-08-13 12:07:07 -0700

    errors

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        for item in row:
-            print("{:d}".format(item), end="\t")
+        for i in range(len(row)):
+            print("{}{:d}".format(("" if i == 0 else " "), row[i]) end='')
         print()
