e656a77cc03835ff0d228a6e73818dd47bbb9708
johnyREx <johnkwofie99@icloud.com>
2023-09-11 13:06:07 -0700

    tell them say we back

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Class MyList that inherits from list
+public method print_sorted that prints
+a sorted version of the list
+"""
+
+class MyList(list):
+    """Methods: print_sorted"""
+
+    def print_sorted(self):
+        """Sorts items in list"""
+        print(sorted(self))
