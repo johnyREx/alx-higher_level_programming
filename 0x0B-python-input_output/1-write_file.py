02be59b40465e8ad7f2a72a3e913df96151ceec9
johnyREx <johnkwofie99@icloud.com>
2023-09-12 13:00:37 -0700

    task 1 error fix

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,34 +1,14 @@
 #!/usr/bin/python3
 """
-This function writes a string
-into a text
+Opens and writes into
+a file
 """
 
 
 def write_file(filename="", text=""):
     """
-    Write a string to a text file.
-
-    Args:
-        filename (str, optional):
-        text (str, optional):
-
-    Returns:
-        int: The number of characters.
+    Open and write file
+    Must use with
     """
-    try:
-        with open(filename, "W", encoding="utf-8") as file:
-            num_characters_written = file.write(text)
-        return num_characters_written
-    except FileNotFoundError:
-        with open(filename, "x", encoding="utf-8") as file:
-            num_characters_written = file.write(text)
-        return num_characters_written
-    except Exception as e:
-        return 0
-
-
-if __name__ == "__main__":
-
-    result = write_file("example.txt", "Hello, World!")
-    print(f"Number of characters written: {result}")
+    with open(filename, 'w+') as file:
+        return(file.write(text))
