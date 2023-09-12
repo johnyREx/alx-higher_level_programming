a8e4488e8d3ef6d8bacab982aed2b026b02cd766
johnyREx <johnkwofie99@icloud.com>
2023-09-12 12:30:21 -0700

    i shall and i will

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Converts from json
+to object
+"""
+
+
+import json
+
+
+def from_json_string(mystr):
+    """
+    Converts mystr
+    """
+   
+   return(json.loads(mystr))
