2b412840b770f161945ecf1cc71e09f590974492
johnyREx <johnkwofie99@icloud.com>
2023-10-17 09:11:40 -0700

    error please leave me alone

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,7 +1,7 @@
 -- Create second table and insert values.
 CREATE TABLE IF NOT EXISTS second_table (
-	id, INT,
-	name VARCHAR(26),
+	id INT,
+	name VARCHAR(256),
 	score INT
 );
 INSERT INTO second_table (id, name, score) VALUES (1, 'John', 10);
