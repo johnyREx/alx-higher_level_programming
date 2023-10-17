7122eb0b013ca1dae20e4852892f69f73240e308
johnyREx <johnkwofie99@icloud.com>
2023-10-17 09:09:32 -0700

    errors everywhere

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,5 +1,9 @@
 -- Create second table and insert values.
-CREATE TABLE IF NOT EXISTS second_table (id, INT, name VARCHAR(26), score INT);
+CREATE TABLE IF NOT EXISTS second_table (
+	id, INT,
+	name VARCHAR(26),
+	score INT
+);
 INSERT INTO second_table (id, name, score) VALUES (1, 'John', 10);
 INSERT INTO second_table (id, name, score) VALUES (2, 'Alex', 3);
 INSERT INTO second_table (id, name, score) VALUES (3, 'Bob', 14);
