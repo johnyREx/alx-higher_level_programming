b6c641f1039d3fa42e2998098b15e977537fa64f
johnyREx <johnkwofie99@icloud.com>
2023-10-18 10:50:55 -0700

    states table

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,8 @@
+-- Create database
+-- Create table.
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa
+USE hbtn_0d_usa
+CREATE TABLE IF NOT EXISTS states (
+	id INT UNIQUE NOT NULL AUTO_INCREMENT,
+	name VARCHAR(256)
+);
