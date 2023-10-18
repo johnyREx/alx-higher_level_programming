e37cb37262e3b5f6b02d78668ae0e4c50a0c2109
johnyREx <johnkwofie99@icloud.com>
2023-10-18 11:21:14 -0700

    fixed a minor error

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,6 +1,7 @@
 -- lists all the cities of California
 USE hbtn_0d_usa;
+
 SELECT cities.*
 FROM cities
 WHERE state_id = (SELECT id FROM states WHERE name = 'California')
-ORDER BY cities.id
+ORDER BY cities.id;
