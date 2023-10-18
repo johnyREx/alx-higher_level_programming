2a153855e00889ca22d157b2d7c74325fdef8cf4
johnyREx <johnkwofie99@icloud.com>
2023-10-18 13:39:42 -0700

    you are wonderful

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,8 +1,7 @@
 -- lists all shows in hbtn_0d_tvsows that have at least 1 genre linked
-SELECT tv_genres.name AS genre,
-	COUNT(tv_show_genres.genre_id) AS number_shows
+SELECT tv_genres.name AS genre, COUNT(tv_show_genres.show_id) AS number_shows
 FROM tv_show_genres
 LEFT JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
-GROUP BY genre
-HAVING number_of_shows > 0
+GROUP BY tv_genres.name
+HAVING COUNT (tv_show_genres.show_id) > 0
 ORDER BY number_shows DESC;
