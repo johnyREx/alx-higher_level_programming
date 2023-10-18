3fa2bab487165045ed5b2d89b365c9d5ee5cd59e
johnyREx <johnkwofie99@icloud.com>
2023-10-18 11:43:27 -0700

    task 9

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -2,6 +2,8 @@
 
 USE hbtn_0d_usa;
 
-SELECT cities.id, cities.name, (SELECT name FROM states WHERE id = cities.state_id) AS state_name
+SELECT cities.id, cities.name, states.name
 FROM cities
+INNER JOIN states
+ON states.id = cities.state_id
 ORDER BY cities.id;
