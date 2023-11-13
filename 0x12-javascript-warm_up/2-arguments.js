717e1d64516f08587ccd98e83ed16c0baf42c624
johnyREx <johnkwofie99@icloud.com>
2023-11-13 06:28:15 -0800

    arguments found

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const numArgs = process.argv.lenght - 2;
+if (numArgs === 0)
+{
+	console.log('No Argument');
+}
+else if (numArgs === 1)
+{
+	console.log('Argument found');
+}
+else
+{
+	console.log('Arguments found');
+}
