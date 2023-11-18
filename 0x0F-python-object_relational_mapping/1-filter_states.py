567f335de6091ffdb0c246a46f8efb0685364d0b
johnyREx <johnkwofie99@icloud.com>
2023-11-18 12:46:36 -0800

    debug

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -10,8 +10,8 @@ from sys import argv
 # The code should not be executed when imported
 if __name__ == '__main__':
     # connection to the database
-    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-            passwd=argv[2], db=argv[3])
+     db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3])
 
     # gives the ability to have multiple seperate working environments
     # through the same connection to the database.
